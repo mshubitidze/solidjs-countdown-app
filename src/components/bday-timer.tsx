ce08cd75b78a5091fbec7387f7443e90f75ec412
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2023-07-02 23:18:37 +0400

    fix time

--- src/components/bday-timer.tsx
+++ src/components/bday-timer.tsx
@@ -5,7 +5,9 @@ const BdayTimer: Component<{}> = ({}) => {
   const [currentDate, setCurrentDate] = createSignal(
     Math.floor(Date.now() / 1000)
   );
-  const bday = Math.floor(new Date("2023-07-03").getTime() / 1000);
+  const bday = Math.floor(
+    new Date("2023-07-03T00:00:00+04:00").getTime() / 1000
+  );
 
   createEffect(() => {
     const interval = setInterval(() => {
@@ -15,19 +17,19 @@ const BdayTimer: Component<{}> = ({}) => {
     onCleanup(() => clearInterval(interval));
   });
 
-  const leftPad = (n: number, p: number, ch: string) => {
-    return n.toLocaleString().padStart(p, ch);
-  };
-
   const formatCountdown = (remainingTime: number) => {
-    const seconds = leftPad(Math.round(remainingTime % 60), 2, "0");
-    const minutes = leftPad(Math.floor((remainingTime / 60) % 60), 2, "0");
-    const hours = leftPad(Math.floor((remainingTime / 3600) % 24), 2, "0");
-    const days = leftPad(Math.floor(remainingTime / 86400), 2, "0");
+    const seconds = Math.round(remainingTime % 60);
+    const minutes = Math.floor((remainingTime / 60) % 60);
+    const hours = Math.floor((remainingTime / 3600) % 24);
+    const days = Math.floor(remainingTime / 86400);
+
+    const padded = [seconds, minutes, hours, days].map((el) =>
+      el.toLocaleString().padStart(2, "0")
+    );
 
     return (
       <div class="flex flex-row items-center gap-3 justify-center md:gap-4">
-        {[days, hours, minutes, seconds].map((el, i) => (
+        {padded.reverse().map((el, i) => (
           <>
             <p class="flex items-center justify-center w-[50px] md:w-[100px]">
               {el}
